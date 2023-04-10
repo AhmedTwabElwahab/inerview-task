7543a52aa24f442300bd286c367fa0d203acd99a
Ahmed Twab El Wahab <128147750+AhmedTwabElwahab@users.noreply.github.com>
2023-04-10 14:16:45 +0200

    create readme.md file

--- resources/views/layouts/navBar.blade.php
+++ resources/views/layouts/navBar.blade.php
@@ -23,6 +23,9 @@
                     <a class="nav-link" href="{{route('category.index')}}"> Category </a>
                 </li>
                 <li class="nav-item">
+                    <a class="nav-link" href="{{route('offer.index')}}"> Offer </a>
+                </li>
+                <li class="nav-item">
                     <a class="nav-link" href="{{route('showCart')}}"> Shopping cart </a>
                 </li>
             </ul>
